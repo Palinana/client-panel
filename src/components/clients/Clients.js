7691140e5cea7691a1334c1fbf1354c512ba22d3
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-08-14 22:00:37 -0400

    adds form to add a client

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,61 +1,85 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
-  render() {
-    const { clients } = this.props; //pulling out clients from the props
-    
-    if(clients){
-        return (
-            <div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <h2>
-                            {' '}
-                            <i className="fas fa-users"> Clients{' '}</i>
-                        </h2>
+
+    constructor(){
+        super();
+        this.state = {
+            totalOwed: null
+        }
+    }
+    //get the clients first and then loop through them to get the total - componentsWillRecieveProps
+    static getDerivedStateFromProps(props, state) {
+        const { clients } = props;
+
+        if(clients) {
+            const total = clients.reduce((total, client) => {
+                return total + parseFloat(client.balance.toString());
+            }, 0);
+            return { totalOwed: total } //setting on the state
+        }
+        return null;
+    }
+
+    render() {
+        const { clients } = this.props; //pulling out clients from the props
+        const { totalOwed } = this.state;
+
+        if(clients){
+            return (
+                <div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h2>
+                                {' '}
+                                <i className="fas fa-users"> Clients{' '}</i>
+                            </h2>
+                        </div>
+                        <div className="col-md-6">
+                            <h5 className="text-right text-secondary"> Total Owed{' '}
+                                <span className="text-primary">${parseFloat(totalOwed).toFixed(2)}</span>
+                            </h5>
+                        </div>
                     </div>
-                    <div className="col-md-6">
-                    </div>
-                </div>
-                <table className="table table-striped">
-                    <thead className="thead-inverse">
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Balance</th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {clients.map(client => (
-                            <tr key={client.id}>
-                                <td>{client.firstName} {client.lastName}</td>
-                                <td>{client.email}</td>
-                                <td>${parseFloat(client.balance).toFixed(2)}</td>
-                                <td>
-                                    <Link to={`/client/${client.id}`} 
-                                        className="btn btn-secondary btn-sm"
-                                    >
-                                    <i className="fas fa-arrow-circle-right"></i> Details
-                                    </Link>
-                                </td>
+                    <table className="table table-striped">
+                        <thead className="thead-inverse">
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Balance</th>
+                                <th />
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-          )
+                        </thead>
+                        <tbody>
+                            {clients.map(client => (
+                                <tr key={client.id}>
+                                    <td>{client.firstName} {client.lastName}</td>
+                                    <td>{client.email}</td>
+                                    <td>${parseFloat(client.balance).toFixed(2)}</td>
+                                    <td>
+                                        <Link to={`/client/${client.id}`} 
+                                            className="btn btn-secondary btn-sm"
+                                        >
+                                        <i className="fas fa-arrow-circle-right"></i> Details
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                )
+        }
+        else {
+            return <Spinner/>
+        }
     }
-    else {
-        return <Spinner/>
-    }
-  }
 }
 
 Clients.propTypes = {
